1f7b82a09b05e085f3d430e21220f18dead6f7a7
Dominik Böhm <dominik.boehm@documatrix.com>
2025-01-20 10:10:32 +0100

    added link content for PDF/UA

--- include/hpdf_structure_element.h
+++ include/hpdf_structure_element.h
@@ -50,8 +50,9 @@ HPDF_StructureElement_AddMarkedContentSequence  (HPDF_StructureElement structure
 
 HPDF_STATUS
 HPDF_StructureElement_SetObjectReference (HPDF_StructureElement structure_element,
-                                         HPDF_Page             page,
-                                         HPDF_Annotation       annot);
+                                          HPDF_Page             page,
+                                          HPDF_Annotation       annot,
+                                          const char           *content);
 
 HPDF_STATUS
 HPDF_StructureElement_SetAlternateText  (HPDF_StructureElement  structure_element,
